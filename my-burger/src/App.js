799d8239c6b0492ee9e98c3355d89742b7faf4a2
CodeSlip <jason@rawlings.io>
2018-09-04 11:47:59 -0400

    Added lazy loading

--- my-burger/src/App.js
+++ my-burger/src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import Layout from "./hoc/Layout/Layout";
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from './containers/Checkout/Orders/Orders';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
+const asyncCheckout = asyncComponent(() => {
+  return import ('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import ('./containers/Checkout/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import ('./containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -20,7 +29,7 @@ class App extends Component {
   render() {
     let routes = (
         <Switch>
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
@@ -29,10 +38,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
